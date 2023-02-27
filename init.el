0ded34b1a41c348be1aaf2c220a560bb94072a45
Adolph Vogel <ajvogel@gmail.com>
2023-02-27 20:56:23 +0200

    Added python configuration from old configuration

--- init.el
+++ init.el
@@ -65,6 +65,87 @@ scroll-conservatively 1000)
     :config
     (ivy-posframe-mode 1))
 
+;;--------------------------------[ Projectile ]-------------------------------
+(use-package projectile
+  :diminish projectile-mode ;; Think this makes things not show in mode-line
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
+
+
+;;----------------------------------[ Python ]---------------------------------
+(use-package python
+  :config
+  ;; Remove guess indent python message
+  (setq python-indent-guess-indent-offset-verbose nil))
+
+(use-package company
+  :ensure t
+  :defer t
+  :custom
+  ;; Search other buffers with the same modes for completion instead of
+  ;; searching all other buffers.
+  (company-dabbrev-other-buffers t)
+  (company-dabbrev-code-other-buffers t)
+  ;; M-<num> to select an option according to its number.
+  (company-show-numbers t)
+  ;; Only 2 letters required for completion to activate.
+  (company-minimum-prefix-length 3)
+  ;; Do not downcase completions by default.
+  (company-dabbrev-downcase nil)
+  ;; Even if I write something with the wrong case,
+  ;; provide the correct casing.
+  (company-dabbrev-ignore-case t)
+  ;; company completion wait
+  (company-idle-delay 0.2)
+  ;; No company-mode in shell & eshell
+  (company-global-modes '(not eshell-mode shell-mode))
+  ;; Use company with text and programming modes.
+    :hook ((text-mode . company-mode)
+           (prog-mode . company-mode)))
+
+;; Supposedly, makes company look a little nicer.
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(use-package eglot
+  :ensure t
+  :defer t
+  :hook (python-mode . eglot-ensure))
+
+(use-package pipenv
+  :hook (python-mode . pipenv-mode)
+  :init
+  (
+   setq pipenv-projectile-after-switch-function #'pipenv-projectile-after-switch-extended))
+
+(use-package rainbow-delimiters
+  :after python-mode
+  :hook (python-mode . rainbow-delimiters-mode))
+
+(use-package indent-guide
+  :after python-mode
+  :hook (python-mode . indent-guide-mode))
+
+(use-package python-cell
+  :after python-mode
+  :hook (python-mode . python-cell-mode))
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    ;; (hs-minor-mode)
+	    (outline-minor-mode)
+	    (local-set-key (kbd "C-.") 'python-indent-shift-right)
+	    (local-set-key (kbd "C-,") 'python-indent-shift-left)
+	    (local-set-key (kbd "M-n") 'python-cell-forward-cell)
+	    (local-set-key (kbd "M-p") 'python-cell-backward-cell)
+	    (local-set-key (kbd "C--") 'outline-hide-subtree)
+	    (local-set-key (kbd "C-=") 'outline-show-subtree)	    
+	    (setq python-shell-interpreter "ipython" python-shell-interpreter-args "--simple-prompt -i")
+	    (local-set-key (kbd "C-c i") 'run-python) ;; Python Shell
+	    (setq-default py-split-windows-on-execute-function 'split-window-horizontally)
+	    ))
 
 ;;---------------------------[ Custom Set Variables ]--------------------------
 (custom-set-variables
@@ -72,10 +153,13 @@ scroll-conservatively 1000)
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
+ '(column-number-mode t)
+ '(global-display-line-numbers-mode t)
+ '(package-selected-packages '(use-package))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Fira Code" :foundry "CTDB" :slant normal :weight normal :height 113 :width normal)))))
